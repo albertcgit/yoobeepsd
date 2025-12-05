8e5711cc8d0a9607acc1d9cccd188b717ce6022b
albertcgit <albertcanoza@gmail.com>
2025-12-06 08:52:00 +1300

    Update W3A1 File Processing.py

--- W3A1 File Processing.py
+++ W3A1 File Processing.py
@@ -12,7 +12,7 @@ class FileProcessing:
 if __name__ == "__main__":
 
     object = FileProcessing()
-    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8", errors="ignore")
+    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8")
     object.read_and_print(input_file)
-    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8", errors="ignore")
+    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8")
     object.count_asterisk(input_file)

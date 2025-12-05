2fe9a7f25654adbd77fc4e6303387c7d0ff1eea2
albertcgit <albertcanoza@gmail.com>
2025-12-06 08:49:22 +1300

    Added Week 3 Activity 1

---
+++ W3A1 File Processing.py
@@ -0,0 +1,18 @@
+class FileProcessing:
+    def read_and_print(self, input_file):
+        content = input_file.read()
+        print(content)
+        input_file.close()
+
+    def count_asterisk(self, input_file):
+        content = input_file.read()
+        print(f"The number of * is {content.count("*")}")
+        input_file.close()
+
+if __name__ == "__main__":
+
+    object = FileProcessing()
+    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8", errors="ignore")
+    object.read_and_print(input_file)
+    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8", errors="ignore")
+    object.count_asterisk(input_file)

4b8d3c32374c6d8934b51e73bf2bb0fb489801ef
albertcgit <albertcanoza@gmail.com>
2025-12-06 09:12:21 +1300

    Update W3A1 File Processing.py

--- W3A1 File Processing.py
+++ W3A1 File Processing.py
@@ -1,20 +1,18 @@
 class FileProcessing:
     def read_and_print(self, input_file):
-        content = input_file.read()
-        print(content)
+        with open(input_file, "r", encoding="utf-8") as f:
+            content = f.read()
+            print(content)
 
     def count_asterisk(self, input_file):
-        content = input_file.read()
-        print(f"The number of * is {content.count("*")}")
+        with open(input_file, "r", encoding="utf-8") as f:
+            content = f.read()
+            print(f"The number of * is {content.count("*")}")
 
 if __name__ == "__main__":
 
     object = FileProcessing()
 
-    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8")
+    input_file = r"C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt"
     object.read_and_print(input_file)
-    input_file.close()
-
-    input_file = open("C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_file.txt", "r", encoding="utf-8")
     object.count_asterisk(input_file)
-    input_file.close()

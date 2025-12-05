c555ffd6e0e20e1e4c2a5ae294c845f8e785c72f
albertcgit <albertcanoza@gmail.com>
2025-12-06 09:26:06 +1300

    Added W3A2

---
+++ W3A2 File Processing - Write and Append.py
@@ -0,0 +1,17 @@
+class FileProcessing:
+    def append_and_print(self, input_file):
+        # Append "Hello World!" at the end
+        with open(input_file, "a", encoding="utf-8") as f:
+            f.write("End of File")
+
+        # Read and print the entire file
+        with open(input_file, "r", encoding="utf-8") as f:
+            content = f.read()
+            print(content)
+
+if __name__ == "__main__":
+
+    object = FileProcessing()
+
+    input_file = r"C:\\Users\\Albert\\Documents\\1_Yoobee Files\\1_Professional Software Development\\Week 3\\demo_text.txt"
+    object.append_and_print(input_file)
